40604f17f4601655ca302dec2618fc02c2800503
Ayk1255 <kakojan.ajk@gmail.com>
2026-09-09 00:14:10 +0300

    Add new sum function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def greet(name):
     return f"Привет, {name}! Это мой первый коммит из PyCharm."
 
+def sum(a,b):
+    return a+b
+
 
 if __name__ == "__main__":
     user_name = input("Введите ваше имя: ")
     print(greet(user_name))
+    a = int(input("Первое число"))
+    b = int(input("Второе число"))
+    print(sum(a,b))

455c63ee523cfa5daf932e6a25c890afbad8c5d9
Ayk1255 <kakojan.ajk@gmail.com>
2026-09-09 00:26:43 +0300

    Add int in function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def greet(name):
     return f"Привет, {name}! Это мой первый коммит из PyCharm."
 
-def sum(a,b):
+def sum(a:int,b:int):
     return a+b
 
 
 if __name__ == "__main__":
     user_name = input("Введите ваше имя: ")
     print(greet(user_name))
-    a = int(input("Первое число"))
-    b = int(input("Второе число"))
+    a = input("Первое число")
+    b = input("Второе число")
     print(sum(a,b))
